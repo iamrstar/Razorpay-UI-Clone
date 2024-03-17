15b3269812973d8b1612f613dc2a4e87277b6e85
Raj Chatterjee <85397058+iamrstar@users.noreply.github.com>
2024-03-17 10:19:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily:{
+      mullish: ["Mulish", "Sans-serif"],
+    },
+
+    colors: {
+      deepgreen: "#006241"
+    },},
+  },
+  plugins: [],
+}
